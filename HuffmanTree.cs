31ed36b921594becdbb869df3b657501d29ee0ca
Delan Azabani <delan@azabani.com>
2014-04-19 05:48:48 +0800

    partial commit of HuffmanTree

---
+++ HuffmanTree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asgn
+{
+    /// <summary>
+    /// Class representing a node in a Huffman tree.
+    /// </summary>
+    class HuffmanTreeNode : IComparable<HuffmanTreeNode>
+    {
+        public byte[] symbol;
+        public double frequency;
+        HuffmanTreeNode parent;
+
+        public int CompareTo(HuffmanTreeNode other)
+        {
+            int retval = 0;
+            if (frequency < other.frequency)
+                retval = -1;
+            if (frequency > other.frequency)
+                retval = 1;
+            return retval;
+        }
+    }
+
+    /// <summary>
+    /// Class representing a Huffman tree.
+    /// </summary>
+    class HuffmanTree
+    {
+        MinHeap<HuffmanTreeNode> pq = new MinHeap<HuffmanTreeNode>();
+
+        public HuffmanTree(FrequencyTable table)
+        {
+            foreach (var pair in table.freq)
+            {
+                HuffmanTreeNode node = new HuffmanTreeNode() {
+                    symbol = pair.Key,
+                    frequency = pair.Value
+                };
+                pq.insert(node);
+            }
+        }
+    }
+}
