915fb0c9ca82e11f17948f1af26b2fa011397cdb
Delan Azabani <delan@azabani.com>
2014-04-19 06:16:37 +0800

    implement HuffmanTree* (untested)

--- HuffmanTree.cs
+++ HuffmanTree.cs
@@ -6,13 +6,13 @@ using System.Text;
 namespace Asgn
 {
     /// <summary>
-    /// Class representing a node in a Huffman tree.
+    /// Class representing a generic node in a Huffman tree.
+    /// This class must be inherited from, not instantiated.
     /// </summary>
-    class HuffmanTreeNode : IComparable<HuffmanTreeNode>
+    abstract class HuffmanTreeNode : IComparable<HuffmanTreeNode>
     {
-        public byte[] symbol;
         public double frequency;
-        HuffmanTreeNode parent;
+        public HuffmanTreeInternalNode parent;
 
         public int CompareTo(HuffmanTreeNode other)
         {
@@ -26,22 +26,56 @@ namespace Asgn
     }
 
     /// <summary>
+    /// Class representing a leaf node in a Huffman tree.
+    /// </summary>
+    class HuffmanTreeLeafNode : HuffmanTreeNode
+    {
+        public byte[] symbol;
+    }
+
+    /// <summary>
+    /// Class representing an internal node in a Huffman tree.
+    /// </summary>
+    class HuffmanTreeInternalNode : HuffmanTreeNode
+    {
+        public HuffmanTreeNode left, right;
+    }
+
+    /// <summary>
     /// Class representing a Huffman tree.
     /// </summary>
     class HuffmanTree
     {
+        HuffmanTreeNode head;
+
         MinHeap<HuffmanTreeNode> pq = new MinHeap<HuffmanTreeNode>();
+        List<HuffmanTreeLeafNode> leaves = new List<HuffmanTreeLeafNode>();
 
         public HuffmanTree(FrequencyTable table)
         {
+            // first insert all leaf nodes into the min-heap
             foreach (var pair in table.freq)
             {
-                HuffmanTreeNode node = new HuffmanTreeNode() {
-                    symbol = pair.Key,
-                    frequency = pair.Value
+                HuffmanTreeLeafNode node = new HuffmanTreeLeafNode() {
+                    frequency = pair.Value,
+                    symbol = pair.Key
                 };
                 pq.insert(node);
+                leaves.Add(node);
             }
+            // then insert (n - 1) internal nodes
+            for (int i = 0; i < pq.length; i++)
+            {
+                HuffmanTreeInternalNode node = new HuffmanTreeInternalNode();
+                node.left = pq.remove();
+                node.right = pq.remove();
+                node.frequency = node.left.frequency + node.right.frequency;
+                node.left.parent = node;
+                node.right.parent = node;
+                pq.insert(node);
+            }
+            // now the min node is the root of the tree
+            head = pq.remove();
         }
     }
 }
