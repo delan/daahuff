36f119e93883e77195cb24545feec24aad7e6598
Delan Azabani <delan@azabani.com>
2014-04-19 06:29:46 +0800

    HuffmanEncoder stub

---
+++ HuffmanEncoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asgn
+{
+    /// <summary>
+    /// Static class containing Huffman (de)compression methods.
+    /// </summary>
+    static class HuffmanEncoder
+    {
+        public static byte[] deflate(byte[] input, FrequencyTable table)
+        {
+            BitArray output = new BitArray();
+            HuffmanTree tree = new HuffmanTree(table);
+            return null;
+        }
+    }
+}
